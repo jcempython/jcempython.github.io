b686d261b3e357fbb557d97f1b7a5c4defed910a
Julio Melanda <jcemelanda@gmail.com>
2022-08-30 23:36:02 +0200

    Adds circleci

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'http://jcempython.github.io'
+SITEURL = 'https://jcempython.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
